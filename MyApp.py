af38d5bc446d1e5ee293dafaab28367a2f2665a3
Vu Thi Thanh <thanhvt234112e@st.uel.edu.vn>
2024-11-16 22:14:04 +0700

    Add logic for BMI project

--- MyApp.py
+++ MyApp.py
@@ -1,9 +1,8 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
 from MainWindowEx import MainWindowEx
-
 app=QApplication([])
 myWindow= MainWindowEx()
 myWindow.setupUi(QMainWindow())
-myWindow.show()
+Window.show()
 app.exec()
